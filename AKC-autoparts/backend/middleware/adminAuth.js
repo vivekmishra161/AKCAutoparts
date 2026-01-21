95d67174124b71fe851243f9a40d9b11178d1ec8
vivekmishra161 <vivekmishra261105@gmail.com>
2026-01-22 02:25:00 +0530

    Delete AKC-autoparts directory

--- AKC-autoparts/backend/middleware/adminAuth.js
+++
@@ -1,6 +0,0 @@
-module.exports = function adminAuth(req, res, next) {
-  if (!req.session.user || req.session.user.role !== 'admin') {
-    return res.redirect('/admin/login');
-  }
-  next();
-};
--- AKC-autoparts/backend/routes/admindashboard.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const adminAuth = require('../middleware/adminAuth');
-
-router.get('/dashboard', adminAuth, (req, res) => {
-  res.render('admin/dashboard');
-});
-
-module.exports = router;
